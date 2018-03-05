9f86689989c5839c74c1a7fd86f522ff9e24748e
Aellon Krider <aellonkrider@gmail.com>
2018-03-05 13:50:47 -0500

    Update add_peak.rb and peaks_controller.rb

--- app/controllers/peaks_controller.rb
+++ app/controllers/peaks_controller.rb
@@ -1,2 +1,20 @@
 class PeakController < ApplicationController
+
+	get '/peaks' do
+		if logged_in?
+	      @peaks = Peak.all
+	      erb :'peaks/peaks'
+	    else
+	      redirect to '/login'
+	    end
+	end
+
+	get '/peaks/new' do
+		if logged_in?
+			@peaks = Peak.all
+      		erb :'/peaks/add_peak'
+    	else
+      		redirect to '/login'
+    	end
+	end
 end
