e3a154fee8c9472f53307f668574cc149b94c00b
Aellon Krider <aellonkrider@gmail.com>
2018-03-18 22:17:07 -0400

    Update peaks controller with case statement to include elevation, update dropdown menu in add_peak.erb

--- app/controllers/peaks_controller.rb
+++ app/controllers/peaks_controller.rb
@@ -23,6 +23,79 @@ class PeakController < ApplicationController
 	    if params[:name] == "" || params[:elevation] == "" || params[:date_hiked] == ""
 	      redirect to "/peaks/new"
 	    else
+	    	case params[:name]
+	    	when "Balsam"
+	    		params[:elevation] = 3600
+			when "Balsam Cap"
+	    		params[:elevation] = 3623
+	    	when "Balsam Lake"
+	    		params[:elevation] = 3723
+	    	when "Bearpen"
+	    		params[:elevation] = 3600
+	    	when "Big Indian"
+	    		params[:elevation] = 3700
+	    	when "Black Dome"
+	    		params[:elevation] = 3980
+	    	when "Blackhead"
+	    		params[:elevation] = 3940
+	    	when "Cornell"
+	    		params[:elevation] = 3860
+	    	when "Doubletop"
+	    		params[:elevation] = 3860
+	    	when "Eagle"
+	    		params[:elevation] = 3600
+	    	when "Fir"
+	    		params[:elevation] = 3620
+	    	when "Friday"
+	    		params[:elevation] = 3694
+	    	when "Graham"
+	    		params[:elevation] = 3868
+	    	when "Halcott"
+	    		params[:elevation] = 3537
+	    	when "Hunter"
+	    		params[:elevation] = 4040
+	    	when "Indian Head"
+	    		params[:elevation] = 3573
+	    	when "Kaaterskill High Peak"
+	    		params[:elevation] = 3655
+	    	when "Lone"
+	    		params[:elevation] = 3721
+	    	when "North Dome"
+	    		params[:elevation] = 3610
+	    	when "Panther"
+	    		params[:elevation] = 3720
+	    	when "Peekamoose"
+	    		params[:elevation] = 3843
+	    	when "Plateau"
+	    		params[:elevation] = 3840
+	    	when "Rocky"
+	    		params[:elevation] = 3508
+	    	when "Rusk"
+	    		params[:elevation] = 3680
+	    	when "Southwest Hunter"
+	    		params[:elevation] = 3740
+	    	when "Sherrill"
+	    		params[:elevation] = 3540
+	    	when "Slide"
+	    		params[:elevation] = 4180
+	    	when "Sugarloaf"
+	    		params[:elevation] = 3880
+	    	when "Table"
+	    		params[:elevation] = 3847
+	    	when "Thomas Cole"
+	    		params[:elevation] = 3940
+	    	when "Twin"
+	    		params[:elevation] = 3640
+	    	when "Vly"
+	    		params[:elevation] = 3529
+	    	when "Westkill"
+	    		params[:elevation] = 3880
+	    	when "Windham High Peak"
+	    		params[:elevation] = 3524
+	    	when "Wittenberg"
+	    		params[:elevation] = 3780
+	    	end
+	    	
 	      @peak = current_user.peaks.create(name: params[:name], elevation: params[:elevation], date_hiked: params[:date_hiked], remarks: params[:remarks])
 	      redirect to "/peaks/#{@peak.id}"
 	    end
