e5bf77af62c7d92dae7adffdf739da550860303f
Aellon Krider <aellonkrider@gmail.com>
2018-03-22 10:42:35 -0400

    Update secret session key to be more secure in application_controller, update views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "secret"
+    set :session_secret, "60d8e5ced05c26c86cc87850fb4b484227b42c074e63c3ed07a8fcef7cf010548d411d9e527783daa8e7f62e9cb50a95e8c67b9d5aff31b6c61035ae7bc22b7d"
     use Rack::Flash
   end
 
